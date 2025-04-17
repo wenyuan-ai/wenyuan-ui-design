489a4c5b187737d4c588f9a386e6f48919a5f79e
OneDay AI 代码助手 <oneday_bot@alibaba-inc.com>
2025-04-17 15:29:05 +0800

    18Be4j0c_siJbkwM_nl2sql-ui-tech_问渊-科技风格界面优化

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Chat from './components/Chat';
 const App = () => {
   return (
     <div className="app">
+      <div className="tech-orb tech-orb-1"></div>
+      <div className="tech-orb tech-orb-2"></div>
       <Sidebar />
       <Chat />
     </div>
