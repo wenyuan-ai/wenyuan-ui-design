223d9d8586d14ca9b5bd6ad25ab15baae81db0ed
OneDay AI 代码助手 <oneday_bot@alibaba-inc.com>
2025-04-24 10:14:02 +0800

    18Be4lqz_jB2SXJe_fix-react-error_修复 React 重复声明问题

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
-import Chat from './components/Chat';
-
-import React from 'react';
 import Auth from './components/Auth';
 
 const App = () => {
   return (
     <div className="app">
       <Auth />
-    </div>
-  );
-  return (
-    <div className="app">
-      <div className="tech-orb tech-orb-1"></div>
-      <div className="tech-orb tech-orb-2"></div>
-      <Sidebar />
-      <Chat />
     </div>
   );
 };
